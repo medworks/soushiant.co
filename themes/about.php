e264d8b9bda0522137c7ef37ccb460152b0a7d72
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-11-09 12:28:57 +0330

    add about page

--- themes/about.php
+++ themes/about.php
@@ -5,7 +5,7 @@ $html=<<<cd
 		<div class="sixteen columns">
 			<!-- Page Title -->
 			<div id="page-title">
-				<h2>About</h2>
+				<h2>درباره ما</h2>
 				<div id="bolded-line"></div>
 			</div>
 			<!-- Page Title / End -->
@@ -14,20 +14,15 @@ $html=<<<cd
 		<div class="container">
 			<!-- Standard Structure -->
 			<div class="two-thirds column">
-				<div class="headline no-margin"><h4>Our Approach</h4></div>
-				<p>Donec odio ut arcu fringilla dictum eu eu nisl. Donec rutrum erat non arcu gravida porttitor. Nunc et magna nisi.Aliquam at erat in purus aliquet mollis. Fusce elementum velit vel dolor iaculis egestas. Maecenas ut nulla quis eros scelerisque posuere vel vitae nibh. Proin id condimentum sem. Morbi vitae dui in magna vestibulum suscipit vitae vel nunc. Integer ut risus nulla. malesuada tortor, nec scelerisque lorem mattis.</p>
-				<p>Nunc et rutrum consetetur sadipscing elitr, sed diam nonumy at volutpat. Sed consectetur suscipit lorem nunc.adipiscing elit. Integer commodo tristique odio, quis fringilla ligula aliquet ut. Maecenas sed justo varius velit imperdiet bibendum</p>
-				<p>Integer commodo tristique odio, quis fringilla ligula aliquet ut. Maecenas sed justo varius velit imperdiet bibendum. Vivamus nec sapien massa, a imperdiet diam. Aliquam erat volutpat. Sed tetur suscipit nunc et rutrum. </p>
+				<div class="headline no-margin"><h4>درباره ما</h4></div>
+				<p>درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... </p>
+				<p>درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... </p>
+				<p>درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... </p>
 			</div>
 			<div class="one-third column">
-				<div class="headline no-margin"><h4>Careers</h4></div>
-				<!-- Large Notice -->
-				<div class="large-notice">
-					<h2>We're Hiring</h2>
-					<p>We're offering great oppurtunites for Project Manager and Web Designer.</p>
-					<a href="#" class="button medium color">Apply</a>
-				</div>
-				<p></p>
+				<div class="headline no-margin"><h4>رویکرد ما</h4></div>
+				<p>درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... </p>
+				<p>درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... درباهر شرکت... </p>
 			</div>
 			<!-- Standard Structure End -->
 		</div>
