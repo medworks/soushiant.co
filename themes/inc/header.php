8c0fd8e43850eb503ea1cb835d6297a36e45c3b3
Saeid Hatami <hatami4510@gmail.com>
2013-11-17 18:30:39 +0330

    fix some error

--- themes/inc/header.php
+++ themes/inc/header.php
@@ -119,7 +119,7 @@
 						<li><a href="single_project.html">Single Project</a></li>
 					</ul> -->
 				</li>
-				<li><a href="projects.html">پروژه ها</a></li>
+				<li><a href="works.html">پروژه ها</a></li>
 				<li><a href="news.html">اخبار</a></li>
 				<li><a href="contact.html">تماس با ما</a></li>
 			</ul>
