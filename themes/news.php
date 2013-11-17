c727697d6dd599be324fbcf2f4813b4f77e09d34
saeid hatami <hatami4510@gmail.com>
2013-11-17 16:13:28 +0330

    fix & completting codes

--- themes/news.php
+++ themes/news.php
@@ -34,7 +34,7 @@ foreach($news as $key => $post)
 $html.=<<<cd
 				<!-- Post -->
 				<div class="post">
-					<div class="post-img picture"><a href="blog_post.html"><img src="themes/images/demo/blog-img-01.jpg" alt=""><div class="image-overlay-link"></div></a></div>
+					<div class="post-img picture"><a href="blog_post.html"><img src="{$post[image]}" alt="{$post[subject]}"><div class="image-overlay-link"></div></a></div>
 					<a href="#" class="post-icon standard"></a>
 					<div class="post-content">
 						<div class="post-title"><h2><a href="news-fullpage1.html">{$post["subject"]}</a></h2></div>
@@ -47,22 +47,11 @@ $html.=<<<cd
 						<a href="news-fullpage{$post[id]}.html" class="button color">ادامه خبر</a>
 					</div>
 				</div>
+				<!-- Post -->
 cd;
 }
 $html.=<<<cd
-				<!-- Post -->
-				<div class="post">
-					<div class="post-img picture"><a href="blog_post.html"><img src="themes/images/demo/blog-img-01.jpg" alt=""><div class="image-overlay-link"></div></a></div>
-					<a href="#" class="post-icon standard"></a>
-					<div class="post-content">
-						<div class="post-title"><h2><a href="blog_post.html">خبر اول</a></h2></div>
-						<div class="post-meta rtl"><span><i class="mini-ico-calendar"></i>تاریخ: 1392/12/11</span> <span><i class="mini-ico-user"></i>به وسیله: مجتبی امجدی</span> <!-- <span><i class="mini-ico-comment"></i>With <a href="#">12 Comments</a></span> --></div>
-						<div class="post-description">
-							<p>شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... </p>
-						</div>
-						<a href="#" class="button color">ادامه خبر</a>
-					</div>
-				</div>
+				
 				<ul class="pagination rtl">
 					<a href="#"><li>1</li></a>
 					<a href="#"><li class="current">2</li></a>
