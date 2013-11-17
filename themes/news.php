969b3b283e6e2a82a960af7bb694ed9c05fce6ac
saeid hatami <hatami4510@gmail.com>
2013-11-17 16:08:01 +0330

    completting codes

--- themes/news.php
+++ themes/news.php
@@ -37,12 +37,14 @@ $html.=<<<cd
 					<div class="post-img picture"><a href="blog_post.html"><img src="themes/images/demo/blog-img-01.jpg" alt=""><div class="image-overlay-link"></div></a></div>
 					<a href="#" class="post-icon standard"></a>
 					<div class="post-content">
-						<div class="post-title"><h2><a href="news-fullpage1.html">خبر اول</a></h2></div>
-						<div class="post-meta rtl"><span><i class="mini-ico-calendar"></i>تاریخ: 1392/12/11</span> <span><i class="mini-ico-user"></i>به وسیله: مجتبی امجدی</span> <!-- <span><i class="mini-ico-comment"></i>With <a href="#">12 Comments</a></span> --></div>
+						<div class="post-title"><h2><a href="news-fullpage1.html">{$post["subject"]}</a></h2></div>
+						<div class="post-meta rtl"><span><i class="mini-ico-calendar"></i>تاریخ: {$ndate}</span> 
+						<span><i class="mini-ico-user"></i>به وسیله:  {$post["userid"]}</span> 
+						<!-- <span><i class="mini-ico-comment"></i>With <a href="#">12 Comments</a></span> --></div>
 						<div class="post-description">
-							<p>شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... شرح توضیحات... </p>
+						<p>{$post["body"]}</p>
 						</div>
-						<a href="#" class="button color">ادامه خبر</a>
+						<a href="news-fullpage{$post[id]}.html" class="button color">ادامه خبر</a>
 					</div>
 				</div>
 cd;
