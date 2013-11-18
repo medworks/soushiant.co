03f0db7dd9a9828b3064157b85e4a26de11d2df1
mojiAm <amjadi.mojtaba@gmail.com>
2013-11-18 11:56:19 -0800

    fix menu

--- classes/functions.php
+++ classes/functions.php
@@ -18,6 +18,14 @@
 				$seo->Site_Describtion = mb_substr(GetSettingValue('About_System',1),0,150,"UTF-8");
                 return "themes/about.php";
 			break;
+			case 'adsl':
+				$seo->Site_Title = "ADSL";	
+                return "themes/adsl.php";
+			break;
+			case 'services':
+				$seo->Site_Title = "ADSL";	
+                return "themes/services.php";
+			break;
 			case 'contact':			    			
 				$seo->Site_Title = "تماس با ما";
 				$seo->Site_Describtion = mb_substr(GetSettingValue('Address',1),0,150,"UTF-8");
