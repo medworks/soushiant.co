2c6687457ec293022d917301ed576dc107321d22
mojiAm <amjadi.mojtaba@gmail.com>
2013-11-19 11:04:48 -0800

    change folder of partner image

--- themes/inc/partner.php
+++ themes/inc/partner.php
@@ -4,11 +4,11 @@
 			<!-- Headline -->
 			<div class="headline no-margin"><h3>همکاران ما</h3></div>
 			<ul class="client-list">
-				<li><img src="themes/images/demo/logo-01.png" alt=""></li>
-				<li><img src="themes/images/demo/logo-02.png" alt=""></li>
-				<li><img src="themes/images/demo/logo-03.png" alt=""></li>
-				<li><img src="themes/images/demo/logo-04.png" alt=""></li>
-				<li><img src="themes/images/demo/logo-05.png" alt=""></li>
+				<li><img src="themes/images/partner/logo-01.png" alt=""></li>
+				<li><img src="themes/images/partner/logo-02.png" alt=""></li>
+				<li><img src="themes/images/partner/logo-03.png" alt=""></li>
+				<li><img src="themes/images/partner/logo-04.png" alt=""></li>
+				<li><img src="themes/images/partner/logo-05.png" alt=""></li>
 			</ul>	
 		</div>
 	</div>
