3edbb5e185a322f6227188266a2830dcc5ab357c
Saeid Hatami <hatami4510@gmail.com>
2013-11-21 13:03:56 +0330

    fix seo error

--- themes/main.php
+++ themes/main.php
@@ -1,5 +1,9 @@
 <?php
-	include_once('inc/header.php');
-	include_once('inc/index-content.php');
+	ob_start();
+	include_once("inc/index-content.php");
+	$page_content = ob_get_contents();
+	ob_end_clean();	
+	include_once("inc/header.php");
+	echo $page_content;	
 	include_once('inc/footer.php');
 ?>
