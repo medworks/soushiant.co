8c808687889385c8d6416592740e9d020b43eb8e
Saeid Hatami <hatami4510@gmail.com>
2013-12-18 21:00:43 +0330

    changing codes part 2

--- manager/companymgr.php
+++ manager/companymgr.php
@@ -259,38 +259,36 @@ $html = $code;
 else 
 if ($_GET["item"]=="gplanmgr")
 {
-	if (!$overall_error && $_POST["mark"]=="savecomp")
+	if (!$overall_error && $_POST["mark"]=="savegplan")
 	{	    
-		$fields = array("`name`","`body`");
-		$_POST["body"] = addslashes($_POST["body"]);		
-		$values = array("'{$_POST[name]}'","'{$_POST[body]}'");
-		if (!$db->InsertQuery('company',$fields,$values)) 
+		$fields = array("`compid`,`subject`");				
+		$values = array("'{$_POST[cbcomp]}'","'{$_POST[subject]}'");
+		if (!$db->InsertQuery('plangroups',$fields,$values)) 
 		{
 			//$msgs = $msg->ShowError("ثبت اطلاعات با مشکل مواجه شد");
-			header('location:?item=compmgr&act=new&msg=2');			
-			//$_GET["item"] = "compmgr";
+			header('location:?item=gplanmgr&act=new&msg=2');			
+			//$_GET["item"] = "gplanmgr";
 			//$_GET["act"] = "new";
 			//$_GET["msg"] = 2;
 		} 	
 		else 
 		{  										
 			//$msgs = $msg->ShowSuccess("ثبت اطلاعات با مو??قیت انجام شد");			
-			header('location:?item=compmgr&act=new&msg=1');		    
-			//$_GET["item"] = "compmgr";
+			header('location:?item=gplanmgr&act=new&msg=1');		    
+			//$_GET["item"] = "gplanmgr";
 			//$_GET["act"] = "new";
 			//$_GET["msg"] = 1;
 		}  				 
 	}
     else
-	if (!$overall_error && $_POST["mark"]=="editcomp")
-	{		
-	    $_POST["body"] = addslashes($_POST["body"]);	    
-		$values = array("`name`"=>"'{$_POST[name]}'",
-			            "`body`"=>"'{$_POST[body]}'");
+	if (!$overall_error && $_POST["mark"]=="editgplan")
+	{			    	    
+		$values = array("`compid`"=>"'{$_POST[cbcomp]}'",
+			            "`subject`"=>"'{$_POST[subject]}'");
 			
-        $db->UpdateQuery("company",$values,array("id='{$_GET[cid]}'"));
+        $db->UpdateQuery("plangroups",$values,array("id='{$_GET[pid]}'"));
 		header('location:?item=compmgr&act=mgr');
-		//$_GET["item"] = "compmgr";
+		//$_GET["item"] = "gplanmgr";
 		//$_GET["act"] = "act";			
 	}
 	if ($_GET['act']=="new")
@@ -298,54 +296,61 @@ if ($_GET["item"]=="gplanmgr")
 		$editorinsert = "
 			<p>
 				<input type='submit' id='submit' value='ذخیره' class='submit' />	 
-				<input type='hidden' name='mark' value='savecomp' />";
+				<input type='hidden' name='mark' value='savegplan' />";
 	}
 	if ($_GET['act']=="edit")
 	{	
-		$row=$db->Select("company","*","id='{$_GET["cid"]}'",NULL);
+		$row=$db->Select("plangroups","*","id='{$_GET["pid"]}'",NULL);
 		$editorinsert = "
 		<p>
 			 <input type='submit' id='submit' value='ویرایش' class='submit' />	 
-			 <input type='hidden' name='mark' value='editcomp' />";
+			 <input type='hidden' name='mark' value='editgplan' />";
 	}
 	if ($_GET['act']=="del")
 	{
-		$db->Delete("company"," id",$_GET["cid"]);
-		if ($db->CountAll("company")%10==0) $_GET["pageNo"]-=1;		
-		header("location:?item=compmgr&act=mgr&pageNo={$_GET[pageNo]}");
+		$db->Delete("plangroups"," id",$_GET["pid"]);
+		if ($db->CountAll("plangroups")%10==0) $_GET["pageNo"]-=1;		
+		header("location:?item=gplanmgr&act=mgr&pageNo={$_GET[pageNo]}");
 	}	
 if ($_GET['act']=="new" or $_GET['act']=="edit")
 {
+	$comp = $db->SelectAll("company","*",null,"id ASC");
+	if ($_GET['act']=="edit")
+		$cbcomp = DbSelectOptionTag("cbcomp",$comp,"name","{$row[compid]}",null,"select validate[required]");
+	else
+		$cbcomp = DbSelectOptionTag("cbcomp",$comp,"name",null,null,"select validate[required]");
 $msgs = GetMessage($_GET['msg']);
 $html=<<<cd
 	<script type='text/javascript'>
 		$(document).ready(function(){	   
-			$("#frmcompanymgr").validationEngine();			
+			$("#frmgplanmgr").validationEngine();			
 		});
 	</script>
   <div class="title">
       <ul>
         <li><a href="adminpanel.php?item=dashboard&act=do">پیشخوان</a></li>
-	    <li><span>مدیریت شرکت</span></li>
+	    <li><span>مدیریت گروهبندی پلان</span></li>
       </ul>
       <div class="badboy"></div>
   </div>
   <div class="mes" id="message">{$msgs}</div>
   <div class='content'>
-	<form name="frmcompanymgr" id="frmcompanymgr" class="" action="" method="post" >
+	<form name="frmgplanmgr" id="frmgplanmgr" class="" action="" method="post" >
      <p class="note">پر کردن موارد مشخص شده با * الزامی می باشد</p>	 
        <div class="badboy"></div>
        <p>
-         <label for="name">عنوان شرکت</label>
+         <label for="name">نام شرکت</label>         
+         <span>*</span>
+       </p>
+		{$cbcomp}
+		<div class="badboy"></div>
+       <p>
+         <label for="name">عنوان گروه</label>         
          <span>*</span>
        </p>    
        <input type="text" name="name" class="validate[required] subject" id="name" value='{$row[name]}'/>   	  
 	   <div class="badboy"></div>
-  	   <p>
-         <label for="body">توضیحات</label>
-         <span>*</span>
-       </p>
-       <textarea cols="50" rows="10" name="body" class="body" id="body" > {$row[body]}</textarea>  	   
+  	    	   
 	   {$editorinsert}
       	 <input type="reset" value="پاک کردن" class='reset' />
        </p>  
@@ -357,10 +362,10 @@ cd;
 } else
 if ($_GET['act']=="mgr")
 {
-	if ($_POST["mark"]=="srhnews")
+	if ($_POST["mark"]=="srhgplan")
 	{	 			    
 		$rows = $db->SelectAll(
-				"company",
+				"plangroups",
 				"*",
 				"{$_POST[cbsearch]} LIKE '%{$_POST[txtsrh]}%'",
 				null,
@@ -368,17 +373,17 @@ if ($_GET['act']=="mgr")
 				10);
 			if (!$rows) 
 			{					
-				//$_GET['item'] = "compmgr";
+				//$_GET['item'] = "gplansmgr";
 				//$_GET['act'] = "mgr";
 				//$_GET['msg'] = 6;				
-				header("Location:?item=compmgr&act=mgr&msg=6");
+				header("Location:?item=gplanmgr&act=mgr&msg=6");
 			}
 		
 	}
 	else
 	{	
 		$rows = $db->SelectAll(
-				"company",
+				"plangroups",
 				"*",
 				null,
 				null,
@@ -387,12 +392,9 @@ if ($_GET['act']=="mgr")
     }
                 $rowsClass = array();
                 $colsClass = array();
-                $rowCount =($_GET["rec"]=="all" or $_POST["mark"]!="srhnews")?$db->CountAll("company"):Count($rows);
+                $rowCount =($_GET["rec"]=="all" or $_POST["mark"]!="srhgplan")?$db->CountAll("plangroups"):Count($rows);
                 for($i = 0; $i < Count($rows); $i++)
                 {						
-					$rows[$i]["body"] =(mb_strlen($rows[$i]["body"])>50)?
-					mb_substr(html_entity_decode(strip_tags($rows[$i]["body"]), ENT_QUOTES, "UTF-8"), 0, 50,"UTF-8") . "..." :
-					html_entity_decode(strip_tags($rows[$i]["body"]), ENT_QUOTES, "UTF-8");						
 					if ($i % 2==0)
 					 {
 							$rowsClass[] = "datagridevenrow";
@@ -401,13 +403,13 @@ if ($_GET['act']=="mgr")
 					{
 							$rowsClass[] = "datagridoddrow";
 					}					
-					$rows[$i]["edit"] = "<a href='?item=compmgr&act=edit&cid={$rows[$i]["id"]}' class='edit-field'" .
+					$rows[$i]["edit"] = "<a href='?item=gplanmgr&act=edit&pid={$rows[$i]["id"]}' class='edit-field'" .
 							"style='text-decoration:none;'></a>";								
 					$rows[$i]["delete"]=<<< del
 					<a href="javascript:void(0)"
 					onclick="DelMsg('{$rows[$i]['id']}',
 						'از حذف این خبر اطمینان دارید؟',
-					'?item=compmgr&act=del&pageNo={$_GET[pageNo]}&cid=');"
+					'?item=gplanmgr&act=del&pageNo={$_GET[pageNo]}&pid=');"
 					 class='del-field' style='text-decoration:none;'></a>
 del;
                 }
@@ -416,16 +418,15 @@ del;
             if (Count($rows) > 0)
             {                    
                     $gridcode .= DataGrid(array( 
-					        "name"=>"نام شرکت",
-							"body"=>"توضیحات",							
+					        "compid"=>"نام شرکت",
+							"name"=>"نام گروه طرح",							
                             "edit"=>"ویرایش",
 							"delete"=>"حذف",), $rows, $colsClass, $rowsClass, 10,
-                            $_GET["pageNo"], "id", false, true, true, $rowCount,"item=compmgr&act=mgr");
+                            $_GET["pageNo"], "id", false, true, true, $rowCount,"item=gplanmgr&act=mgr");
                     
             }
 $msgs = GetMessage($_GET['msg']);
-$list = array("name"=>"نام شرکت",
-              "body"=>"توضیحات");
+$list = array("name"=>"نام گروه طرح");
 $combobox = SelectOptionTag("cbsearch",$list,"name");
 $code=<<<edit
 <script type='text/javascript'>
@@ -439,20 +440,20 @@ $code=<<<edit
 					<div class="title">
 				      <ul>
 				        <li><a href="adminpanel.php?item=dashboard&act=do">پیشخوان</a></li>
-					    <li><span>مدیریت شرکت</span></li>
+					    <li><span>مدیریت گروهبندی طرح ها</span></li>
 				      </ul>
 				      <div class="badboy"></div>
 				  </div>
                     <div class="Top">                       
 						<center>
-							<form action="?item=compmgr&act=mgr" method="post" id="frmsrh" name="frmsrh">
+							<form action="?item=gplanmgr&act=mgr" method="post" id="frmsrh" name="frmsrh">
 								<p>جستجو بر اساس {$combobox}</p>
 								<p class="search-form">
 									<input type="text" id="txtsrh" name="txtsrh" class="search-form" value="جستجو..." onfocus="if (this.value == 'جستجو...') {this.value = '';}" onblur="if (this.value == '') {this.value = 'جستجو...';}"  /> 																	
-									<a href="?item=compmgr&act=mgr" name="srhsubmit" id="srhsubmit" class="button"> جستجو</a>
-									<a href="?item=compmgr&act=mgr&rec=all" name="retall" id="retall" class="button"> کلیه اطلاعات</a>
+									<a href="?item=gplanmgr&act=mgr" name="srhsubmit" id="srhsubmit" class="button"> جستجو</a>
+									<a href="?item=gplanmgr&act=mgr&rec=all" name="retall" id="retall" class="button"> کلیه اطلاعات</a>
 								</p>
-								<input type="hidden" name="mark" value="srhnews" /> 
+								<input type="hidden" name="mark" value="srhgplan" /> 
 								{$msgs}
 								{$gridcode} 
 							</form>
@@ -461,8 +462,7 @@ $code=<<<edit
 
 edit;
 $html = $code;
-}			
-			
+}						
 }
    
 return $html;
