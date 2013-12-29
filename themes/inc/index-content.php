78f89a8310b3b192d241bf0839226548ae331021
Saeid Hatami <hatami4510@gmail.com>
2013-12-29 19:17:27 +0330

    add link to slides in index page

--- themes/inc/index-content.php
+++ themes/inc/index-content.php
@@ -50,15 +50,16 @@ cd;
 foreach($slides as $key=>$val)
 {
 $html.=<<<cd
-				<!-- Slide #1 -->
 				<div class="ls-layer" style="transition2d: 67; timeshift: -1000; slidedelay: 7000;">	 
 					<!-- Background -->
-					<img class="ls-bg" src="{$val[image]}" alt="{$val[subject]}">
+					<a href="{$val[link]}" target="_blank">					
+						<img class="ls-bg" src="{$val[image]}" alt="{$val[subject]}">
+					</a>					
 					<div class="slide-caption ls-s1" style="left: 20px; top: 228px; width: 30%; slidedirection: bottom; slideoutdirection: bottom; durationin: 800; durationout: 800; delayin: 0; delayout: 0;">
 						<h3>{$val["subject"]}</h3>
 						<p>{$val["body"]}</p>
 					</div>
-				</div>
+				</div>				
 cd;
 }	
 $html.=<<<cd
