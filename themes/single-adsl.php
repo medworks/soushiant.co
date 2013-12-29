ed96fa78046eaee4792a7646421b9b11e57ea231
Saeid Hatami <hatami4510@gmail.com>
2013-12-29 20:45:07 +0330

    fix codes

--- themes/single-adsl.php
+++ themes/single-adsl.php
@@ -1,14 +1,16 @@
 <?php
   include_once("./classes/database.php");
+  include_once("./classes/functions.php");
   include_once("./lib/persiandate.php");
   $db = Database::GetDatabase();   
-  $company = $db->SelectAll("company","*");   
+  $gplans = $db->SelectAll("plangroups","*","compid = '{$_GET[wid]}'");
+  $compname = GetCompanyName($_GET["wid"]); 
 $html=<<<cd
 		<div class="container">
 			<div class="sixteen columns">
 				<!-- Page Title -->
 				<div id="page-title">
-					<h2>ADSL / شاتل</h2>
+					<h2>ADSL / {$compname}</h2>
 					<div id="bolded-line"></div>
 				</div>
 				<!-- Page Title / End -->
@@ -17,11 +19,9 @@ $html=<<<cd
 		<div class="container cyan">			
 cd;
 $i = 0;
-foreach($company as $key => $val)
+foreach($gplans as $key => $val)
 {
-   ++$i;
-   $val["body"]=  strip_tags($val["body"]);
-   $val["body"] = (mb_strlen($val["body"])>200) ? mb_substr($val["body"],0,200,"UTF-8")."..." : $val["body"];
+   ++$i;      
    if ($i % 2 != 0)	
 	$html.="<!-- Icon Box Container --> <div class='icon-boxes-container'> ";
 $html.=<<<cd
@@ -29,13 +29,12 @@ $html.=<<<cd
 				<div class="eight columns">
 					<div class="icon-box">
 						<i class="ico-wifi-alt" style="margin-left: -12px;"></i>
-						<h3><a class="price-table" name="{$val[name]}" id="{$val[id]}">{$val["name"]}</a></h3>
-						<p>{$val["body"]}</p>
+						<h3><a class="price-table" name="{$val[subject]}" id="{$val[id]}">{$val["subject"]}</a></h3>						
 					</div>
 				</div>
 				<!-- Icon Box End -->
 cd;
-if (($i % 2 == 0) or (count($company)==$i))	  
+if (($i % 2 == 0) or (count($gplans)==$i))	  
 		$html.="</div>   <!-- Icon Box Container / End -->";
    
 }
