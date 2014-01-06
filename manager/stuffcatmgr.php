9ebf65d153997990c5c340cb4fb25d5c2611415b
Saeid Hatami <hatami4510@gmail.com>
2014-01-06 17:49:07 +0330

    fixing codes #1

--- manager/stuffcatmgr.php
+++ manager/stuffcatmgr.php
@@ -12,18 +12,17 @@
 		die(); // solve a security bug
 	}
 	$db = Database::GetDatabase();
-	$sess = Session::GetSesstion();
 	$overall_error = false;
-   if ($_GET['item']=="catmgr")
+   if ($_GET['item']=="stuffcatmgr")
    {
 	if (!$overall_error && $_POST["mark"]=="savecat")
 	{	    
 		$fields = array("`secid`","`catname`","`latinname`","`describe`");		
 		$values = array("'{$_POST[cbsec]}'","'{$_POST[catname]}'","'{$_POST[latinname]}'","'{$_POST[describe]}'");		
-		if (!$db->InsertQuery('category',$fields,$values)) 
+		if (!$db->InsertQuery('stuffcat',$fields,$values)) 
 		{
 			//$msgs = $msg->ShowError("ثبت اطلاعات با مشکل مواجه شد");
-			header('location:?item=catmgr&act=new&msg=2');
+			header('location:?item=stuffcatmgr&act=new&msg=2');
 			//exit();
 			//$_GET["item"] = "catmgr";
 			//$_GET["act"] = "new";
@@ -32,7 +31,7 @@
 		else 
 		{  										
 			//$msgs = $msg->ShowSuccess("ثبت اطلاعات با مو??قیت انجام شد");
-			header('location:?item=catmgr&act=new&msg=1');
+			header('location:?item=stuffcatmgr&act=new&msg=1');
 			//$_GET["item"] = "catmgr";
 			//$_GET["act"] = "new";
 			//$_GET["msg"] = 1;
@@ -46,8 +45,8 @@
 		                "`catname`"=>"'{$_POST[catname]}'",
 						"`latinname`"=>"'{$_POST[latinname]}'",
 						"`describe`"=>"'{$_POST[describe]}'");		
-        $db->UpdateQuery("category",$values,array("id='{$_GET["cid"]}'"));
-		header('location:?item=catmgr&act=mgr');
+        $db->UpdateQuery("stuffcat",$values,array("id='{$_GET["cid"]}'"));
+		header('location:?item=stuffcatmgr&act=mgr');
 		//$_GET["item"] = "catmgr";
 		//$_GET["act"] = "mgr";		
 	}
@@ -70,7 +69,7 @@ if ($_GET['act']=="new")
 }
 if ($_GET['act']=="edit")
 {
-	$row=$db->Select("category","*","id='{$_GET["cid"]}'",NULL);
+	$row=$db->Select("stuffcat","*","id='{$_GET["cid"]}'",NULL);
 	$editorinsert = "
 	<p>
       	 <input type='submit' id='submit' value='ویرایش' class='submit' />	 
@@ -78,9 +77,9 @@ if ($_GET['act']=="edit")
 }
 if ($_GET['act']=="del")
 {
-	$db->Delete("category"," id",$_GET["cid"]);
-	if ($db->CountAll("category")%10==0) $_GET["pageNo"]-=1;		
-	header("location:?item=catmgr&act=mgr&pageNo={$_GET[pageNo]}");
+	$db->Delete("stuffcat"," id",$_GET["cid"]);
+	if ($db->CountAll("stuffcat")%10==0) $_GET["pageNo"]-=1;		
+	header("location:?item=stuffcatmgr&act=mgr&pageNo={$_GET[pageNo]}");
 }
 if ($_GET['act']=="do")
 {
@@ -95,23 +94,23 @@ if ($_GET['act']=="do")
 		<div class="sub-menu" id="mainnav">
 			<ul class="two-column">
 			<li>		  
-				<a href="?item=secmgr&act=new">سرگروه جدید
+				<a href="?item=stuffsecmgr&act=new">سرگروه جدید
 					<span class="add-headline"></span>
 				</a>
 			  </li>
 			  <li>
-				<a href="?item=secmgr&act=mgr">حذف/ویرایش سرگروه
+				<a href="?item=stuffsecmgr&act=mgr">حذف/ویرایش سرگروه
 					<span class="edit-headline"></span>
 				</a>
 			  </li>
 			  <li>		  
-				<a href="?item=catmgr&act=new">گروه جدید
+				<a href="?item=stuffcatmgr&act=new">گروه جدید
 					<span class="add-cat"></span>
 				</a>
 			  </li>
 			  
 			  <li>
-				<a href="?item=catmgr&act=mgr">حذف/ویرایش گروه ها
+				<a href="?item=stuffcatmgr&act=mgr">حذف/ویرایش گروه ها
 					<span class="edit-cat"></span>
 				</a>
 			  </li>
@@ -123,7 +122,7 @@ ht;
 if ($_GET['act']=="new" or $_GET['act']=="edit")
 {
 $msgs = GetMessage($_GET['msg']);
-$sections = $db->SelectAll("section","*",null,"secname ASC");
+$sections = $db->SelectAll("stuffsec","*",null,"secname ASC");
 if ($_GET['act']=="edit")
 	$cbsection = DbSelectOptionTag("cbsec",$sections,"secname","{$row[secid]}",null,"select validate[required]");
 else
@@ -179,7 +178,7 @@ if ($_GET['act']=="mgr")
 	if ($_POST["mark"]=="srhcat")
 	{
 	   $rows = $db->SelectAll(
-				"category",
+				"stuffcat",
 				"*",
 				"{$_POST[cbsearch]} LIKE '%{$_POST[txtsrh]}%'",
 				"catname ASC",
@@ -190,14 +189,14 @@ if ($_GET['act']=="mgr")
 				//$_GET['item'] = "catmgr";
 				//$_GET['act'] = "mgr";
 				//$_GET['msg'] = 6;				
-				header("Location:?item=catmgr&act=mgr&msg=6");
+				header("Location:?item=stuffcatmgr&act=mgr&msg=6");
 			}
 		
 	}
 	else
 	{	
 		$rows = $db->SelectAll(
-				"category",
+				"stuffcat",
 				"*",
 				null,
 				"catname ASC",
@@ -206,7 +205,7 @@ if ($_GET['act']=="mgr")
     }
                 $rowsClass = array();
                 $colsClass = array();
-                $rowCount =($_GET["rec"]=="all" or $_POST["mark"]!="srhcat")?$db->CountAll("category"):Count($rows);
+                $rowCount =($_GET["rec"]=="all" or $_POST["mark"]!="srhcat")?$db->CountAll("stuffcat"):Count($rows);
                 for($i = 0; $i < Count($rows); $i++)
                 {						
 		        $rows[$i]["catname"] =(mb_strlen($rows[$i]["catname"])>20)?mb_substr($rows[$i]["catname"],0,20,"UTF-8")."...":$rows[$i]["catname"];
@@ -222,13 +221,13 @@ if ($_GET['act']=="mgr")
 						$rowsClass[] = "datagridoddrow";
 				}
 				$rows[$i]["secid"] = GetSectionName($rows[$i]["secid"]);
-				$rows[$i]["edit"] = "<a href='?item=catmgr&act=edit&cid={$rows[$i]["id"]}' class='edit-field' " .
+				$rows[$i]["edit"] = "<a href='?item=stuffcatmgr&act=edit&cid={$rows[$i]["id"]}' class='edit-field' " .
 						"style='text-decoration:none;'></a>";								
 				$rows[$i]["delete"]=<<< del
 				<a href="javascript:void(0)"
 				onclick="DelMsg('{$rows[$i]['id']}',
 					'از حذف این گروه اطمینان دارید؟',
-				'?item=catmgr&act=del&pageNo={$_GET[pageNo]}&cid=');"
+				'?item=stuffcatmgr&act=del&pageNo={$_GET[pageNo]}&cid=');"
 				 class='del-field' style='text-decoration:none;'></a>
 del;
                          }
@@ -243,7 +242,7 @@ del;
 							"describe"=>"توضیحات",							
                             "edit"=>"ویرایش",
 							"delete"=>"حذف",), $rows, $colsClass, $rowsClass, 10,
-                            $_GET["pageNo"], "id", false, true, true, $rowCount,"item=catmgr&act=mgr");
+                            $_GET["pageNo"], "id", false, true, true, $rowCount,"item=stuffcatmgr&act=mgr");
                     
             }
 $msgs = GetMessage($_GET['msg']);
@@ -269,13 +268,13 @@ $code=<<<edit
 				  </div>
                     <div class="Top">                       
 						<center>
-							<form action="?item=catmgr&act=mgr" method="post" id="frmsrh" name="frmsrh">
+							<form action="?item=stuffcatmgr&act=mgr" method="post" id="frmsrh" name="frmsrh">
 								<p>جستجو بر اساس {$combobox}</p>
 
 								<p class="search-form">
 									<input type="text" id="date_input_1" name="txtsrh" class="search-form" value="جستجو..." onfocus="if (this.value == 'جستجو...') {this.value = '';}" onblur="if (this.value == '') {this.value = 'جستجو...';}"  /> 
-									<a href="?item=catmgr&act=mgr" name="srhsubmit" id="srhsubmit" class="button"> جستجو</a>
-									<a href="?item=catmgr&act=mgr&rec=all" name="retall" id="retall" class="button"> کلیه اطلاعات</a>
+									<a href="?item=stuffcatmgr&act=mgr" name="srhsubmit" id="srhsubmit" class="button"> جستجو</a>
+									<a href="?item=stuffcatmgr&act=mgr&rec=all" name="retall" id="retall" class="button"> کلیه اطلاعات</a>
 								</p>
 								<input type="hidden" name="mark" value="srhcat" /> 
 								{$msgs}
@@ -296,10 +295,10 @@ if (!$overall_error && $_POST["mark"]=="savesec")
 	{	    
 		$fields = array("`secname`","`latinname`","`describe`");		
 		$values = array("'{$_POST[secname]}'","'{$_POST[latinname]}'","'{$_POST[describe]}'");		
-		if (!$db->InsertQuery('section',$fields,$values)) 
+		if (!$db->InsertQuery('stuffsec',$fields,$values)) 
 		{
 			//$msgs = $msg->ShowError("ثبت اطلاعات با مشکل مواجه شد");
-			header('location:?item=secmgr&act=new&msg=2');
+			header('location:?item=stuffsecmgr&act=new&msg=2');
 			//$_GET["item"] = "secmgr";
 			//$_GET["act"] = "new";
 			//$_GET["msg"] = 2;			
@@ -307,7 +306,7 @@ if (!$overall_error && $_POST["mark"]=="savesec")
 		else 
 		{  										
 			//$msgs = $msg->ShowSuccess("ثبت اطلاعات با مو??قیت انجام شد");
-			header('location:?item=secmgr&act=new&msg=1');
+			header('location:?item=stuffsecmgr&act=new&msg=1');
 			//$_GET["item"] = "secmgr";
 			//$_GET["act"] = "new";
 			//$_GET["msg"] = 1;
@@ -319,9 +318,9 @@ if (!$overall_error && $_POST["mark"]=="savesec")
 		$values = array("`secname`"=>"'{$_POST[secname]}'",
 						"`latinname`"=>"'{$_POST[latinname]}'",
 						"`describe`"=>"'{$_POST[describe]}'");		
-        $db->UpdateQuery("section",$values,array("id='{$_GET["sid"]}'"));
+        $db->UpdateQuery("stuffsec",$values,array("id='{$_GET["sid"]}'"));
 		echo $db->cmd;
-		header('location:?item=secmgr&act=mgr');
+		header('location:?item=stuffsecmgr&act=mgr');
 		//$_GET["item"] = "secmgr";
 		//$_GET["act"] = "mgr";			
 	}
@@ -343,7 +342,7 @@ if ($_GET['act']=="new")
 }
 if ($_GET['act']=="edit")
 {
-	$row=$db->Select("section","*","id='{$_GET["sid"]}'",NULL);
+	$row=$db->Select("stuffsec","*","id='{$_GET["sid"]}'",NULL);
 	$editorinsert = "
 	<p>
       	 <input type='submit' id='submit' value='ویرایش' class='submit' />	 
@@ -351,9 +350,9 @@ if ($_GET['act']=="edit")
 }
 if ($_GET['act']=="del")
 {
-	$db->Delete("section"," id",$_GET["cid"]);
-	if ($db->CountAll("section")%10==0) $_GET["pageNo"]-=1;		
-	header("location:?item=secmgr&act=mgr&pageNo={$_GET[pageNo]}");
+	$db->Delete("stuffsec"," id",$_GET["cid"]);
+	if ($db->CountAll("stuffsec")%10==0) $_GET["pageNo"]-=1;		
+	header("location:?item=stuffsecmgr&act=mgr&pageNo={$_GET[pageNo]}");
 }
 if ($_GET['act']=="new" or $_GET['act']=="edit")
 {
@@ -404,7 +403,7 @@ if ($_GET['act']=="mgr")
 	if ($_POST["mark"]=="srhcat")
 	{	 		
 	   $rows = $db->SelectAll(
-				"section",
+				"stuffsec",
 				"*",
 				"{$_POST[cbsearch]} LIKE '%{$_POST[txtsrh]}%'",
 				"secname ASC",
@@ -415,14 +414,14 @@ if ($_GET['act']=="mgr")
 				//$_GET['item'] = "secmgr";
 				//$_GET['act'] = "mgr";
 				//$_GET['msg'] = 6;				
-				header("Location:?item=secmgr&act=mgr&msg=6");
+				header("Location:?item=stuffsecmgr&act=mgr&msg=6");
 			}
 		
 	}
 	else
 	{	
 		$rows = $db->SelectAll(
-				"section",
+				"stuffsec",
 				"*",
 				null,
 				"secname ASC",
@@ -431,7 +430,7 @@ if ($_GET['act']=="mgr")
     }
                 $rowsClass = array();
                 $colsClass = array();
-                $rowCount =($_GET["rec"]=="all" or $_POST["mark"]!="srhcat")?$db->CountAll("section"):Count($rows);
+                $rowCount =($_GET["rec"]=="all" or $_POST["mark"]!="srhcat")?$db->CountAll("stuffsec"):Count($rows);
                 for($i = 0; $i < Count($rows); $i++)
                 {						
 		        $rows[$i]["secname"] =(mb_strlen($rows[$i]["secname"])>20)?mb_substr($rows[$i]["secname"],0,20,"UTF-8")."...":$rows[$i]["secname"];
@@ -446,13 +445,13 @@ if ($_GET['act']=="mgr")
 				{
 						$rowsClass[] = "datagridoddrow";
 				}				
-				$rows[$i]["edit"] = "<a href='?item=secmgr&act=edit&sid={$rows[$i]["id"]}' class='edit-field' " .
+				$rows[$i]["edit"] = "<a href='?item=stuffsecmgr&act=edit&sid={$rows[$i]["id"]}' class='edit-field' " .
 						"style='text-decoration:none;'></a>";								
 				$rows[$i]["delete"]=<<< del
 				<a href="javascript:void(0)"
 				onclick="DelMsg('{$rows[$i]['id']}',
 					'از حذف این گروه اطمینان دارید؟',
-				'?item=secmgr&act=del&pageNo={$_GET[pageNo]}&sid=');"
+				'?item=stuffsecmgr&act=del&pageNo={$_GET[pageNo]}&sid=');"
 				 class='del-field' style='text-decoration:none;'></a>
 del;
                          }
@@ -466,7 +465,7 @@ del;
 							"describe"=>"توضیحات",							
                             "edit"=>"ویرایش",
 							"delete"=>"حذف",), $rows, $colsClass, $rowsClass, 10,
-                            $_GET["pageNo"], "id", false, true, true, $rowCount,"item=secmgr&act=mgr");
+                            $_GET["pageNo"], "id", false, true, true, $rowCount,"item=stuffsecmgr&act=mgr");
                     
             }
 $msgs = GetMessage($_GET['msg']);
@@ -492,13 +491,13 @@ $code=<<<edit
 				  </div>
                     <div class="Top">                       
 						<center>
-							<form action="?item=secmgr&act=mgr" method="post" id="frmsrh" name="frmsrh">
+							<form action="?item=stuffsecmgr&act=mgr" method="post" id="frmsrh" name="frmsrh">
 								<p>جستجو بر اساس {$combobox}</p>
 
 								<p class="search-form">
 									<input type="text" id="date_input_1" name="txtsrh" class="search-form" value="جستجو..." onfocus="if (this.value == 'جستجو...') {this.value = '';}" onblur="if (this.value == '') {this.value = 'جستجو...';}"  /> 
-									<a href="?item=secmgr&act=mgr" name="srhsubmit" id="srhsubmit" class="button"> جستجو</a>
-									<a href="?item=secmgr&act=mgr&rec=all" name="retall" id="retall" class="button"> کلیه اطلاعات</a>
+									<a href="?item=stuffsecmgr&act=mgr" name="srhsubmit" id="srhsubmit" class="button"> جستجو</a>
+									<a href="?item=stuffsecmgr&act=mgr&rec=all" name="retall" id="retall" class="button"> کلیه اطلاعات</a>
 								</p>
 								<input type="hidden" name="mark" value="srhcat" /> 
 								{$msgs}
