52c5c8cc81e0c5658620eddb9a94199de5392d67
mojiAm <amjadi.mojtaba@gmail.com>
2014-01-06 18:11:29 -0800

    add print

--- themes/adsl.php
+++ themes/adsl.php
@@ -29,7 +29,7 @@ $html.=<<<cd
 				<div class="eight columns">
 					<div class="icon-box">
 						<i class="ico-wifi-alt" style="margin-left: -12px;"></i>
-						<h3><a href="adsl-fullpage{$val[id]}.html" class="price-table" name="{$val[name]}" id="{$val[id]}">{$val["name"]}</a></h3>
+						<h3><a href="adsl-fullpage{$val[id]}.html" class="price-table" name="{$val[name]}" id="{$val[id]}">{$val["name"]}</a><a class="button">پرینت</a></h3>
 						<p>{$val["body"]}</p>
 					</div>
 				</div>
