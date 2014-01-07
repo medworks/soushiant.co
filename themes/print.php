4ed6d3d9a0f7fed0f68b249a76c8e8cf98fafbf0
mojiAm <amjadi.mojtaba@gmail.com>
2014-01-07 11:37:50 -0800

    add print file

---
+++ themes/print.php
@@ -0,0 +1,48 @@
+<?php
+	header('Content-Type: text/html; charset=UTF-8');
+?>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/print.css">
+</head>
+<body>
+<div class="table-standard">
+	<table width="100%" border="0" cellpadding="0" cellspacing="0">
+		<tbody>
+			<tr>
+				<td class="tdtitle" style="border-left:0 none;" valign="baseline">نام سرویس: ECO128</td>
+				<td colspan="4" class="tdtitle" valign="baseline">سرعت تا:&nbsp; 128/<strong>128</strong>کیلو بیت بر ثانیه</td>
+			</tr>
+			<tr>
+				<td class="tdbox" style="border-bottom:0 none;" valign="baseline" width="*">مدت سرویس</td>
+				<td class="tdbox align-c" style="border-bottom:0 none;" valign="baseline" width="100">یک ماهه</td>
+				<td class="tdbox align-c" style="border-bottom:0 none;" valign="baseline" width="100">سه ماهه</td>
+				<td class="tdbox align-c" style="border-bottom:0 none;" valign="baseline" width="100">شش ماهه</td>
+				<td class="tdbox lcol align-c" style="border-bottom:0 none;" valign="baseline" width="100">یک ساله</td>
+			</tr>
+			<tr>
+				<td class="tdbox" style="border-bottom:0 none;" valign="baseline">میزان ترافیک/ گیگا بایت</td>
+				<td class="tdbox align-c" style="border-bottom:0 none;" valign="baseline">۳</td>
+				<td class="tdbox align-c" style="border-bottom:0 none;" valign="baseline">۶</td>
+				<td class="tdbox align-c" style="border-bottom:0 none;" valign="baseline">۱۰</td>
+				<td class="tdbox lcol align-c" style="border-bottom:0 none;" valign="baseline">۱۵</td>
+			</tr>
+			<tr>
+				<td class="tdbox" style="border-bottom:0 none;" valign="baseline">هزینه اشتراک / تومان</td>
+				<td class="tdbox align-c" style="border-bottom:0 none;" valign="baseline">۱۳٫۹۰۰</td>
+				<td class="tdbox align-c" style="border-bottom:0 none;" valign="baseline">۲۹٫۹۰۰</td>
+				<td class="tdbox align-c" style="border-bottom:0 none;" valign="baseline">۵۲٫۹۰۰</td>
+				<td class="tdbox lcol align-c" style="border-bottom:0 none;" valign="baseline">۸۷٫۹۰۰</td>
+			</tr>
+			<tr>
+				<td class="tdbox even" valign="baseline">متوسط هزینه برای هر ماه / تومان</td>
+				<td class="tdbox even align-c" valign="baseline">۱۳٫۹۰۰</td>
+				<td class="tdbox even align-c" valign="baseline">۹٫۹۰۰</td>
+				<td class="tdbox even align-c" valign="baseline">۸٫۸۰۰</td>
+				<td class="tdbox lcol even align-c" valign="baseline">۷٫۳۰۰</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
+</body>
+</html>
