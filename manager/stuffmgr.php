e33c745f816aff3ea18efa54f1b4cdac41a19e47
Saeid Hatami <hatami4510@gmail.com>
2014-01-08 19:03:02 +0330

    fixing codes #3

--- manager/stuffmgr.php
+++ manager/stuffmgr.php
@@ -14,8 +14,6 @@
 	 }
 	$db = Database::GetDatabase();
 	$overall_error = false;
-if ($_GET['item']=="plansmgr")
-{
 	if ($_GET['item']!="stuffmgr")	exit();
 	if (!$overall_error && $_POST["mark"]=="savestuff")
 	{	    
@@ -120,7 +118,7 @@ $msgs = GetMessage($_GET['msg']);
 $html=<<<cd
 	<script type='text/javascript'>
 		$(document).ready(function(){	   
-			$("#frmplansmgr").validationEngine();
+			$("#frmstuffmgr").validationEngine();
 			$("#cbcomp").change(function(){
 				$.get('ajaxcommand.php?comp='+$(this).val(), function(data) {
 						$('#plans').html(data);
@@ -137,12 +135,12 @@ $html=<<<cd
   </div>
   <div class="mes" id="message">{$msgs}</div>
   <div class='content'>
-	<form name="frmplansmgr" id="frmplansmgr" class="" action="" method="post" >
+	<form name="frmstuffmgr" id="frmstuffmgr" class="" action="" method="post" >
      <p class="note">پر کردن موارد مشخص شده با * الزامی می باشد</p>	 
 	 <div class="badboy"></div>	   
        <div class="badboy"></div>
        <p>
-         <label for="name">نام شرکت</label>
+         <label for="name">گروه</label>
          <span>*</span>
        </p>
 	   {$cbcomp}
@@ -151,50 +149,16 @@ $html=<<<cd
 	   </div>
        <div class="badboy"></div>
        <p>
-         <label for="name">نام طرح</label>
+         <label for="name">نام کالا</label>
          <span>*</span>
        </p>    
        <input type="text" name="name" class="validate[required] subject" id="name" value='{$row[name]}'/>	   
-	   <div class="badboy"></div>
-       <p>
-         <label for="name">سرعت دانلود</label>
-         <span>*</span>
-       </p>    
-       <input type="text" name="speeddl" class="validate[required] subject" id="speeddl" value='{$row[speeddl]}'/> 
-		<div class="badboy"></div>
-       <p>
-         <label for="name">سرعت آپلود</label>
-         <span>*</span>
-       </p>    
-       <input type="text" name="speedup" class="validate[required] subject" id="speedup" value='{$row[speedup]}'/>
-		<div class="badboy"></div>
-       <p>
-         <label for="name">مدت زمان طرح (عدد)</label>
-         <span>*</span>
-       </p>    
-       <input type="text" name="time" class="validate[required] subject" id="time" value='{$row[time]}'/> 
-		<div class="badboy"></div>
-       <p>
-         <label for="name">میزان ترافیک (GB)</label>
-         <span>*</span>
-       </p>    
-       <input type="text" name="trafic" class="validate[required] subject" id="trafic" value='{$row[trafic]}'/>
-		<div class="badboy"></div>
-       <p>
-         <label for="name">هزینه دوره(ریال)</label>
-         <span>*</span>
-       </p>    
-       <input type="text" name="price" class="validate[required] subject" id="price" value='{$row[price]}'/> 	   
-	   <div class="badboy"></div>
+	   <div class='badboy'></div>
   	   <p>
          <label for="detail">توضیحات</label>
          <span>*</span>
        </p>
        <textarea cols="50" rows="10" name="detail" class="detail" id="detail" > {$row[detail]}</textarea>
-       <p>
-         <label for="pos">ترتیب نمایش</label>         
-       </p>
-	   <input type="text" name="pos" class="validate[required] subject" id="name" value='{$row[pos]}'/>	   
 	   {$editorinsert}
       	 <input type="reset" value="پاک کردن" class='reset' />
        </p>  
@@ -325,268 +289,7 @@ $code=<<<edit
 edit;
 $html = $code;
 }
-} else
-if ($_GET['item']=="traficmgr")
-{
-	if (!$overall_error && $_POST["mark"]=="savetrafic")
-	{	    
-		$fields = array("`pid`","`subject`","`cnt`","`price`");		
-		$values = array("'{$_POST[comp]}'","'{$_POST[subject]}'","'{$_POST[count]}'","'{$_POST[price]}'");
-		if (!$db->InsertQuery('trafic',$fields,$values)) 
-		{
-			//$msgs = $msg->ShowError("ثبت اطلاعات با مشکل مواجه شد");
-			header('location:?item=traficmgr&act=new&msg=2');			
-			//$_GET["item"] = "traficmgr";
-			//$_GET["act"] = "new";
-			//$_GET["msg"] = 2;
-		} 	
-		else 
-		{  										
-			//$msgs = $msg->ShowSuccess("ثبت اطلاعات با مو??قیت انجام شد");			
-			header('location:?item=traficmgr&act=new&msg=1');		    
-			//$_GET["item"] = "traficmgr";
-			//$_GET["act"] = "new";
-			//$_GET["msg"] = 1;
-		}  				 
-	}
-    else
-	if (!$overall_error && $_POST["mark"]=="edittrafic")
-	{			    
-		$values = array("`pid`"=>"'{$_POST[comp]}'",
-						"`subject`"=>"'{$_POST[subject]}'",
-		                "`cnt`"=>"'{$_POST[count]}'",
-						"`price`"=>"'{$_POST[price]}'");
-			
-        $db->UpdateQuery("trafic",$values,array("id='{$_GET[tid]}'"));
-		header('location:?item=traficmgr&act=mgr');
-		//$_GET["item"] = "traficmgr";
-		//$_GET["act"] = "act";			
-	}
-	if ($_GET['act']=="new")
-	{
-	    $rows = $db->SelectAll("company","*",null,"id ASC");
-        $cbcomp = DbSelectOptionTag("comp",$rows,"name",null,null,"select validate[required]");
-		$editorinsert = "
-			<p>
-				<input type='submit' id='submit' value='ذخیره' class='submit' />	 
-				<input type='hidden' name='mark' value='savetrafic' />";
-	}
-	if ($_GET['act']=="edit")
-	{	
-		$row = $db->Select("trafic","*","id='{$_GET["tid"]}'",NULL);
-		$comps = $db->SelectAll("company","*",null,"id ASC");
-		$cbcomp = DbSelectOptionTag("comp",$comps,"name","{$row['pid']}",null,"select validate[required]");
-		$editorinsert = "
-		<p>
-			 <input type='submit' id='submit' value='ویرایش' class='submit' />	 
-			 <input type='hidden' name='mark' value='edittrafic' />";
-	}
-	if ($_GET['act']=="del")
-	{
-		$db->Delete("trafic"," id",$_GET["cid"]);
-		if ($db->CountAll("plans")%10==0) $_GET["pageNo"]-=1;		
-		header("location:?item=traficmgr&act=mgr&pageNo={$_GET[pageNo]}");
-	}
-if ($_GET['act']=="do")
-{
-	$html=<<<ht
-		<div class="title">
-	      <ul>
-	        <li><a href="adminpanel.php?item=dashboard&act=do">پیشخوان</a></li>
-	        <li><span>مدیریت طرح ها</span></li>
-	      </ul>
-	      <div class="badboy"></div>
-	    </div>
-		<div class="sub-menu" id="mainnav">
-			<ul>
-			  <li>		  
-				<a href="?item=stuffmgr&act=new">درج طرح جدید
-					<span class="add-plan"></span>
-				</a>
-			  </li>
-			  <li>
-				<a href="?item=stuffmgr&act=mgr" id="news" name="news">حذف/ویرایش طرحها
-					<span class="edit-plan"></span>
-				</a>
-			  </li>
-			  <li>		  
-				<a href="?item=traficmgr&act=new">درج ترافیک
-					<span class="add-plan"></span>
-				</a>
-			  </li>
-			  <li>
-				<a href="?item=traficmgr&act=mgr" >حذف/ویرایش ترافیک
-					<span class="edit-plan"></span>
-				</a>
-			  </li>
-			 </ul>
-			 <div class="badboy"></div>
-		</div>		 
-ht;
-}else
-if ($_GET['act']=="new" or $_GET['act']=="edit")
-{
-$msgs = GetMessage($_GET['msg']);
-$html=<<<cd
-	<script type='text/javascript'>
-		$(document).ready(function(){	   
-			$("#frmtraficmgr").validationEngine();
-    	});
-	</script>
-  <div class="title">
-      <ul>
-        <li><a href="adminpanel.php?item=dashboard&act=do">پیشخوان</a></li>
-	    <li><span>مدیریت طرح ها</span></li>
-      </ul>
-      <div class="badboy"></div>
-  </div>
-  <div class="mes" id="message">{$msgs}</div>
-  <div class='content'>
-	<form name="frmtraficmgr" id="frmtraficmgr" class="" action="" method="post" >
-     <p class="note">پر کردن موارد مشخص شده با * الزامی می باشد</p>	 
-	 <div class="badboy"></div>	   
-       <div class="badboy"></div>
-       <p>
-         <label for="name">نام شرکت</label>
-         <span>*</span>
-       </p>
-	   {$cbcomp}
-       <div class="badboy"></div>
-       <p>
-         <label for="subject">عنوان</label>
-         <span>*</span>
-       </p>    
-       <input type="text" name="subject" class="validate[required] subject" id="subject" value='{$row[subject]}'/>
-	   <div class="badboy"></div>
-       <p>
-         <label for="count">میزان ترافیک (GB)</label>
-         <span>*</span>
-       </p>    
-       <input type="text" name="count" class="validate[required] subject" id="count" value='{$row[cnt]}'/> 
-		<div class="badboy"></div>
-       <p>
-         <label for="price">قیمت (ریال)</label>
-         <span>*</span>
-       </p>    
-       <input type="text" name="price" class="validate[required] subject" id="price" value='{$row[price]}'/>
-	   
-	   {$editorinsert}
-      	 <input type="reset" value="پاک کردن" class='reset' />
-       </p>  
-	</form>
-	<div class='badboy'></div>	
-  </div>  
-   
-cd;
-} else
-if ($_GET['act']=="mgr")
-{
-	if ($_POST["mark"]=="srhtrafic")
-	{	 			    
-		$rows = $db->SelectAll(
-				"trafic",
-				"*",
-				"{$_POST[cbsearch]} LIKE '%{$_POST[txtsrh]}%'",
-				null,
-				$_GET["pageNo"]*10,
-				10);
-			if (!$rows) 
-			{					
-				//$_GET['item'] = "traficmgr";
-				//$_GET['act'] = "mgr";
-				//$_GET['msg'] = 6;				
-				header("Location:?item=traficmgr&act=mgr&msg=6");
-			}
-		
-	}
-	else
-	{	
-		$rows = $db->SelectAll(
-				"trafic",
-				"*",
-				null,
-				null,
-				$_GET["pageNo"]*10,
-				10);
-    }
-                $rowsClass = array();
-                $colsClass = array();
-                $rowCount =($_GET["rec"]=="all" or $_POST["mark"]!="srhtrafic")?$db->CountAll("plans"):Count($rows);
-                for($i = 0; $i < Count($rows); $i++)
-                {						
-				    $rows[$i]["pid"] = GetCompanyName($rows[$i]["pid"]);
-					if ($i % 2==0)
-					 {
-							$rowsClass[] = "datagridevenrow";
-					 }
-					else
-					{
-							$rowsClass[] = "datagridoddrow";
-					}					
-					$rows[$i]["edit"] = "<a href='?item=traficmgr&act=edit&tid={$rows[$i]["id"]}' class='edit-field'" .
-							"style='text-decoration:none;'></a>";								
-					$rows[$i]["delete"]=<<< del
-					<a href="javascript:void(0)"
-					onclick="DelMsg('{$rows[$i]['id']}',
-						'از حذف این خبر اطمینان دارید؟',
-					'?item=traficmgr&act=del&pageNo={$_GET[pageNo]}&tid=');"
-					 class='del-field' style='text-decoration:none;'></a>
-del;
-                }
 
-    if (!$_GET["pageNo"] or $_GET["pageNo"]<=0) $_GET["pageNo"] = 0;
-            if (Count($rows) > 0)
-            {                    
-                    $gridcode .= DataGrid(array( 
-					        "pid"=>"نام شرکت",
-					        "subject"=>"عنوان",
-							"cnt"=>"میزان ( GB )",
-							"price"=>"قیمت ( ریال )",
-                            "edit"=>"ویرایش",
-							"delete"=>"حذف",), $rows, $colsClass, $rowsClass, 10,
-                            $_GET["pageNo"], "id", false, true, true, $rowCount,"item=traficmgr&act=mgr");
-                    
-            }
-$msgs = GetMessage($_GET['msg']);
-$list = array("subject"=>"عنوان",
-              "cnt"=>"میزان",
-			  "price"=>"قیمت");
-$combobox = SelectOptionTag("cbsearch",$list,"subject");
-$code=<<<edit
-<script type='text/javascript'>
-	$(document).ready(function(){	   			
-		$('#srhsubmit').click(function(){	
-			$('#frmsrh').submit();
-			return false;
-		});		
-	});
-</script>	   
-					<div class="title">
-				      <ul>
-				        <li><a href="adminpanel.php?item=dashboard&act=do">پیشخوان</a></li>
-					    <li><span>مدیریت طرح ها</span></li>
-				      </ul>
-				      <div class="badboy"></div>
-				  </div>
-                    <div class="Top">                       
-						<center>
-							<form action="?item=stuffmgr&act=mgr" method="post" id="frmsrh" name="frmsrh">
-								<p>جستجو بر اساس {$combobox}</p>
-								<p class="search-form">
-									<input type="text" id="txtsrh" name="txtsrh" class="search-form" value="جستجو..." onfocus="if (this.value == 'جستجو...') {this.value = '';}" onblur="if (this.value == '') {this.value = 'جستجو...';}"  />									
-									<a href="?item=traficmgr&act=mgr" name="srhsubmit" id="srhsubmit" class="button"> جستجو</a>
-									<a href="?item=traficmgr&act=mgr&rec=all" name="retall" id="retall" class="button"> کلیه اطلاعات</a>
-								</p>
-								<input type="hidden" name="mark" value="srhtrafic" /> 
-								{$msgs}
-								{$gridcode} 
-							</form>
-					   </center>
-					</div>
-edit;
-$html = $code;	
-}
-}
 	
 return $html;
 ?>
