d56d9afed3ba4e5d3975e642053e427416ee9299
mojiAm <amjadi.mojtaba@gmail.com>
2014-08-27 11:59:43 +0330

    change partner

--- themes/inc/partner.php
+++ themes/inc/partner.php
@@ -4,11 +4,11 @@
 			<!-- Headline -->
 			<div class="headline no-margin"><h3>همکاران ما</h3></div>
 			<ul class="client-list">
-				<li><img src="themes/images/partner/logo-01.png" alt=""></li>
-				<li><img src="themes/images/partner/logo-02.png" alt=""></li>
-				<li><img src="themes/images/partner/logo-03.png" alt=""></li>
-				<li><img src="themes/images/partner/logo-04.png" alt=""></li>
-				<li><img src="themes/images/partner/logo-05.png" alt=""></li>
+				<li><a href="http://10.70.246.75:88/Pages/Login.aspx?ReturnUrl=%2fMemberPages%2f" target="_blank" title="اینترنت مخابرات"><img src="themes/images/partner/logo-01.png" alt="اینترنت مخابرات"></a></li>
+				<li><a href="" title=""><img src="themes/images/partner/logo-02.png" target="_blank" alt=""></a></li>
+				<li><a href="http://qt.asiatech.ir/" target="_blank" title="اینترنت آسیاتک"><img src="themes/images/partner/logo-03.png" alt="اینترنت آسیاتک"></a></li>
+				<li><a href="" target="_blank" title=""><img src="themes/images/partner/logo-04.png" alt=""></a></li>
+				<li><a href="" target="_blank" title=""><img src="themes/images/partner/logo-05.png" alt=""></a></li>
 			</ul>	
 		</div>
 	</div>
